8bd916957ac0fac6355c5d84c9c6cb534dd78d2b
tlcgeyer <tamlincupido2402@gmail.com>
2023-11-17 16:05:29 +0200

    question2

--- script/code.js
+++ script/code.js
@@ -1,4 +1,6 @@
 /* Question 1: Use the following id="heading" to change the heading(This is the main heading) of the html to 'Javascript', DO NOT USE QUERY SELECTOR  */
 document.getElementById("heading").innerHTML = "Javascript"; 
 
+/* Question 2: Use the class='paragraph' and change the information to 'This is a language I have mastered and I am proud of myself. I accomplished this because I believed in myself' */
+document.getElementsByClassName("paragraph").innerHTML = "This is a language I have mastered and I am proud of myself.I accomplished this because I believe in myself."
 
