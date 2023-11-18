57e3d5cd7bdc4506362f4ba7fc18345c97141ecc
tlcgeyer <tamlincupido2402@gmail.com>
2023-11-18 20:34:47 +0200

    jsExercise

--- script/code.js
+++ script/code.js
@@ -4,3 +4,6 @@ document.getElementById("heading").innerHTML = "Javascript";
 /* Question 2: Use the class='paragraph' and change the information to 'This is a language I have mastered and I am proud of myself. I accomplished this because I believed in myself' */
 document.querySelector(".paragraph").textContent = "This is a language I have mastered and I am proud of myself. I complished this because I believed in myself.";
 
+/*Question 3: Use the h3 element and change the heading(Subheading) to 'Things I've accomplished so far'. Hint: use querySelector */
+let h3 = document.querySelector("h3")
+h3.innerHTML = ("Things I've accomplished so far.")
